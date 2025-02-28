c73c220d94e57d8667553e0641b37916b0930f10
unknown <ballig360@gmail.com>
2025-02-28 20:23:01 +0530

    fixed a button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
             md:bg-transparent md:static md:mx-0 md:flex gap-6`}>
                 <a href="#About"><li className="text-md transition-all duration-300 p-1 md:p-0">About</li></a>
                 <a href="#Experience"><li className="text-md transition-all duration-300 p-1 md:p-0">Experience</li></a>
-                <a href="#Carousel"><li className="text-md transition-all duration-300 p-1 md:p-0">Projects</li></a>
+                <a href="#Projects"><li className="text-md transition-all duration-300 p-1 md:p-0">Projects</li></a>
                 <a href="#Contact"><li className="text-md transition-all duration-300 p-1 md:p-0">Contact</li></a>
             </ul>
             {showMenu ? (<RiMenu2Line size={30} className="md:hidden absolute right-10 top-6 transition-all duration-300" onClick={()=>{
